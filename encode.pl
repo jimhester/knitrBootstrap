b63871d8cffd94a75c09c37449795bbf52067f3f
Jim Hester <james.f.hester@gmail.com>
2013-04-10 09:09:03 -0400

    Handle relative links in urls for encode

--- encode.pl
+++ encode.pl
@@ -5,7 +5,7 @@ use autodie qw(:all);
 ###############################################################################
 # By Jim Hester
 # Created: 2013 Apr 05 02:45:46 PM
-# Last Modified: 2013 Apr 10 08:52:56 AM
+# Last Modified: 2013 Apr 10 09:13:41 AM
 # Title:encode.pl
 # Purpose: Base64 encode all external links
 ###############################################################################
@@ -13,7 +13,7 @@ use autodie qw(:all);
 ###############################################################################
 use Getopt::Long;
 use Pod::Usage;
-my %args = ( ignore => [ 'mathjax' ] );
+my %args = ( ignore => ['mathjax'] );
 GetOptions( \%args, 'ignore=s@', 'help|?', 'man' ) or pod2usage(2);
 pod2usage(2) if exists $args{help};
 pod2usage( -verbose => 2 ) if exists $args{man};
@@ -29,38 +29,47 @@ use File::MimeInfo;
 my $ua = LWP::UserAgent->new;
 
 #create search regexp
-my @starts = ('script[^<>]+src=', 'link[^<>]+href=', 'url[(]');
-my $search = '((?:' . join('|', map{ "(?:$_)"} @starts) . q{)["']*)([^#"')]+)(["')])};
+my @starts = ( 'script[^<>]+src=', 'link[^<>]+href=', 'url[(]' );
+my $search =
+  '((?:' . join( '|', map {"(?:$_)"} @starts ) . q{)["']*)([^#"')]+)(["')])};
 
-while (my $line = <>) {
-  print encode_string($line, $search);
+while ( my $line = <> ) {
+  print encode_string( $line, $search );
 }
 
 #base64 encode urls or local files recursively
-sub encode_string{
-  my ($string, $search) = @_;
+sub encode_string {
+  my ( $string, $search, $parent_url ) = @_;
+
   #if not in our libraries to ignore
   $string =~ s{$search}{
     my($start, $url, $end) = ($1, $2, $3);
 
     #check for local file
     if(-e $url){
-      my $text = slurp($url);
-      $text = encode_string($text, $search) if $text;
-      my $type = mimetype($url);
-      $url="data:$type;base64," . encode_base64($text, '');
+    my $text = slurp($url);
+    $text = encode_string($text, $search) if $text;
+    my $type = mimetype($url);
+    $url="data:$type;base64," . encode_base64($text, '');
     }
     $url =~ s{^//}{http://}g;
     my $response =$ua->get($url);
+
+    #try appending the parent url to make an absolute url
+    if ( not $response->is_success and defined $parent_url ) {
+      my ($base) = $parent_url =~ m{(.+/)};
+      $response = $ua->get($base.$url);
+    }
     if($response->is_success){
       my $text = $response->decoded_content(charset => 'none');
-      $text = encode_string($text, $search) if $text;
+      $text = encode_string($text, $search, $url) if $text;
       my $type = $response->content_type();
       $url="data:$type;base64," . encode_base64($text, '');
     }
-      "$start$url$end";
-      }eg;
-#}eg;
+    "$start$url$end";
+  }eg;
+
+  #}eg;
   return $string;
 }
 
